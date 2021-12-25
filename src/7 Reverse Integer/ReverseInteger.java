e5b23603cb02d743bb4cce41045eecadeb11db6d
ayotycoon <kaysehventeen@gmail.com>
2021-12-25 02:35:29 -0600

    c

--- src/7 Reverse Integer/ReverseInteger.java
+++ src/7 Reverse Integer/ReverseInteger.java
@@ -1,20 +1,54 @@
 
-public class ReverseInteger{
+public class ReverseInteger {
     // 7 Reverse Integer https://leetcode.com/problems/reverse-integer
 
-    private static void bruteForce(){
+    private static int bruteForce(int x) {
+        int times = x < 0 ? -1 : 1;
+        String str = x + "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
 
-            
+            if (str.charAt(i) == '-') continue;
+            sb.append(str.charAt(i));
+        }
+
+        long revInt = Long.parseLong(sb.toString());
+        if (revInt > Integer.MAX_VALUE) return 0;
+        return (int) (revInt * times);
+
+
     }
-    private static void optimized1(){
 
-            
+    private static int optimized1(int x) {
+
+        boolean isNeg = x < 0 ? true : false;
+        int res = 0;
+        x = Math.abs(x);
+        while (x > 0) {
+            if (res > (Integer.MAX_VALUE / 10)) return 0;
+            res = res * 10 + x % 10;
+            x /= 10;
+            if (res < 0) return 0; // if res go over int limit positive becomes negative
+        }
+
+        res = isNeg ? -1 * res : res;
+
+        return res;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        // bruteForce();
-        // optimized1();
+        int x = 12390;
+
+
+        int bruteForceSolution = bruteForce(x);
+        int optimized1Solution = optimized1(x);
+
+        System.out.println(bruteForceSolution);
+        System.out.println(optimized1Solution);
+
+
+
 
     }
 
--- src/7 Reverse Integer/ReverseInteger.py
+++ src/7 Reverse Integer/ReverseInteger.py
@@ -1,12 +1,42 @@
 
-    
+
 # 7 Reverse Integer https://leetcode.com/problems/reverse-integer
 
-def bruteForce():
-    pass
+def bruteForce(x):
+    times = -1 if x < 0 else 1
+    xstr = str(x) + ""
+    sb = ""
+    i = len(xstr) - 1
+    while (i >= 0):
+        if (xstr[i] == '-'):
+            i -= 1
+            continue
+        sb += xstr[i]
+        i -= 1
+    revInt = int(sb)
+    if (revInt > 2147483647 or revInt < -2147483647):
+        return 0
+    return int((revInt * times))
 
-def optimized1():
-    pass
 
-#bruteForce()
-#optimized1()
+def optimized1(x):
+    isNeg = True if x < 0 else False
+    res = 0
+    x = abs(x)
+    while (x > 0):
+        if res > 2147483647:
+            return 0
+        res = res * 10 + x % 10
+        x = int(x/10)
+        if (res > 2147483647 or res < -2147483647):
+            return 0
+    res = -1 * res if isNeg else res
+    return res
+
+
+x = 214748364
+bruteForceSolution = bruteForce(x)
+optimized1Solution = optimized1(x)
+
+print(bruteForceSolution)
+print(optimized1Solution)
