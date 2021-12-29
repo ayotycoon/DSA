dad5654cbd566fc1cbcdd12c7416f13542218391
ayotycoon <kaysehventeen@gmail.com>
2021-12-28 21:46:40 -0600

    c

--- src/9 Palindrome Number/PalindromeNumber.py
+++ src/9 Palindrome Number/PalindromeNumber.py
@@ -2,11 +2,34 @@
     
 # 9 Palindrome Number https://leetcode.com/problems/palindrome-number
 
-def bruteForce():
-    pass
+def  bruteForce( y) :
+    #            Time O(N)
+    #            Space O(N)
+    _str = str(y) + ""
+    i = 0
+    j = len(_str) - 1
+    while (i <= j) :
+        if (_str[i] != _str[j]) :
+            return  False
+        i += 1
+        j -= 1
+    return  True
 
-def optimized1():
-    pass
+def  optimized1( y) :
+    #            Time O(N)
+    #            Space O(1)
+    if (y < 0) :
+        return  False
+    x = y
+    rev = 0
+    while (x > 0) :
+        rev = (rev * 10) + (x - ((int(x / 10)) * 10))
+        x = (int(x / 10))
+    return  rev == y
 
-#bruteForce()
-#optimized1()
+ 
+x = 121
+bruteForceSolution = bruteForce(x)
+optimized1Solution = optimized1(x)
+print(bruteForceSolution)
+print(optimized1Solution)
