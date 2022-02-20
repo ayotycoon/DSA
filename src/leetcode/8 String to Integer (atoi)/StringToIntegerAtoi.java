e890bc75b3696d409d64fc6f3eefd5240b74d470
ayotycoon <kaysehventeen@gmail.com>
2022-02-20 00:30:55 -0600

    2022-02-20

--- src/leetcode/8 String to Integer (atoi)/StringToIntegerAtoi.java
+++ src/leetcode/8 String to Integer (atoi)/StringToIntegerAtoi.java
@@ -1,21 +1,53 @@
-
-public class StringToIntegerAtoi{
+public class StringToIntegerAtoi {
     // 8 String to Integer (atoi) https://leetcode.com/problems/string-to-integer-atoi
-
-    private static void bruteForce(){
-
-            
+    private static int startFrom(char c) {
+        if(c == '+' || c == '-') return 1;
+        return 0;
     }
-    private static void optimized1(){
+    private static int bruteForce(String s) {
+        if(null == s) return 0;
+        s = s.trim();
+        if(s.length() == 0) return 0;
+        boolean isNegetive = s.charAt(0) == '-';
+        int startFrom = startFrom(s.charAt(0));
+        int length = s.length();
 
+        double result = 0;
+        while(startFrom < length) {
+            char c = s.charAt(startFrom);
+            startFrom++;
+            if(c == ' ') break;
+            if((c - '0') > 9 || c == '.' || c == '+' || c == '-') break;
+            result = result*10 + c - '0';
+        }
+
+        if(isNegetive) result = -result;
+
+        if(result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        else if(Integer.MAX_VALUE < result) return Integer.MAX_VALUE;
+
+        return (int)result;
 
     }
 
-    public static void main(String[] args){
-        System.out.println(Integer.MAX_VALUE);
 
-        // bruteForce();
-        // optimized1();
+    private static int optimized1(String s) {
+     return 0;
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+        for (String s : new String[]{"2147483646"}) {
+            int optimized1Solution = optimized1(s);
+
+
+            System.out.println(optimized1Solution);
+
+        }
+
 
     }
 
