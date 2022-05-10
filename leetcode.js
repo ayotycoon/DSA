3a43207a9625279eb8c7ebf1302224c4f03d497f
ayotycoon <kaysehventeen@gmail.com>
2022-05-10 13:44:06 -0500

    added readme

--- leetcode.js
+++ leetcode.js
@@ -1,20 +1,19 @@
 const path = require('path')
 const fs = require('fs');
-const { doesNotReject } = require('assert');
 
-const relativeDir = '../src/leetcode';
+const relativeDir = './src/leetcode';
 const dir = path.resolve(__dirname, relativeDir);
 
 class LeetcodeFolder {
     number = 0;
     folderName = "";
-    relativeFolderPath="";
+    relativeFolderPath = "";
     relativeReadMeFile = "";
     relativeJavaFile = "";
     relativePythonFile = ""
     constructor(folderName) {
         this.folderName = folderName;
-this.relativeFolderPath = path.join(relativeDir, folderName);
+        this.relativeFolderPath = path.join(relativeDir, folderName);
         // extract number;
         let tempNo = ""
 
@@ -24,11 +23,11 @@ this.relativeFolderPath = path.join(relativeDir, folderName);
         this.number = parseInt(tempNo);
         // extract java files;
         const content = fs.readdirSync(this.relativeFolderPath);
-        
-        for(let file of content){
-if(file.endsWith('.md')) this.relativeReadMeFile = path.join(this.relativeReadMeFile, file);
-if(file.endsWith('.java')) this.relativeJavaFile = path.join(this.relativeFolderPath, file);
-if(file.endsWith('.py')) this.relativePythonFile =  path.join(this.relativeFolderPath, file);
+
+        for (let file of content) {
+            if (file.endsWith('.md')) this.relativeReadMeFile = path.join(this.relativeReadMeFile, file);
+            if (file.endsWith('.java')) this.relativeJavaFile = path.join(this.relativeFolderPath, file);
+            if (file.endsWith('.py')) this.relativePythonFile = path.join(this.relativeFolderPath, file);
         }
     }
 
@@ -39,7 +38,7 @@ if(file.endsWith('.py')) this.relativePythonFile =  path.join(this.relativeFolde
 
 const folders = fs.readdirSync(dir).filter(f => !isNaN(f[0]));
 
-const arr = folders.map(f => new LeetcodeFolder(f)).sort((a,b) => a.number - b.number);
+const arr = folders.map(f => new LeetcodeFolder(f)).sort((a, b) => a.number - b.number);
 
 
 
@@ -49,7 +48,7 @@ let readMe = `
 `;
 
 arr.forEach(clazz => {
-readMe+=`|[${clazz.folderName}](${clazz.relativeReadMeFile}) |[${clazz.relativeJavaFile}](${clazz.relativeJavaFile}) | [${clazz.relativePythonFile}](${clazz.relativePythonFile}) |
+    readMe += `|[${clazz.folderName}](${clazz.relativeReadMeFile}) |[${clazz.relativeJavaFile}](${clazz.relativeJavaFile}) | [${clazz.relativePythonFile}](${clazz.relativePythonFile}) |
 `
 })
 
