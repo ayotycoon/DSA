8e29349f829556da8c505c5745bee9321940217c
ayotycoon <kaysehventeen@gmail.com>
2022-05-11 22:14:48 -0500

    solved no 23

--- src/leetcode/21_Merge_Two_Sorted_Lists/MergeTwoSortedLists.java
+++ src/leetcode/21_Merge_Two_Sorted_Lists/MergeTwoSortedLists.java
@@ -1,3 +1,10 @@
+import LeetcodeTestCase.TestCase;
+import LeetcodeTestCase.TestCaseExecutor;
+import LeetcodeTestCase.TestCaseFunctionGroup;
+import common.ListNode;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 public class MergeTwoSortedLists{
     // 21 Merge Two Sorted Lists https://leetcode.com/problems/merge-two-sorted-lists
@@ -6,17 +13,72 @@ public class MergeTwoSortedLists{
 
             
     }
-    private static void optimized1(){
+    public static void cleaner(ListNode ans, ListNode node){
+        while(node != null){
 
+            ans.next = new ListNode(node.val);
+            ans = ans.next;
+            node = node.next;
+
+        }
+
+    }
+    private static ListNode optimized1(ListNode l1, ListNode l2){
+        ListNode _ans = new ListNode(0);
+        ListNode ans =_ans;
+
+        while(l1 != null && l2 != null){
+
+            if(l1.val < l2.val){
+                ans.next = new ListNode(l1.val);
+                l1 = l1.next;
+            }else {
+                ans.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+
+            ans = ans.next;
+        }
+
+        cleaner(ans, l1);
+        cleaner(ans, l2);
+
+        return _ans.next;
             
     }
 
     public static void main(String[] args){
 
-        // bruteForce();
-        // optimized1();
+        new TestCaseExecutor(
+                computeTestCase(new int[]{1, 2,  4}, new int[]{1, 3,  4})
+
+
+        );
 
     }
+
+    @NotNull
+    private static TestCase computeTestCase(int[] nums1,int[] nums2) {
+        return new TestCase(
+                "%s | %s".formatted(
+                        Arrays.toString(nums1),
+                        Arrays.toString(nums2)
+                ),
+
+/*
+                new TestCaseFunctionGroup("bruteForce", () -> {
+                    var ans = bruteForce(ListNode.create(nums1), ListNode.create(nums2));
+                    return ans != null ? ans.toString() : "";
+                }),
+                */
+
+                new TestCaseFunctionGroup("optimized1", () -> {
+                    var ans = optimized1(ListNode.create(nums1), ListNode.create(nums2));
+                    return ans != null ? ans.toString() : "";
+                })
+        );
+    }
+
 
 
 }
--- src/leetcode/23_Merge_k_Sorted_Lists/MergeKSortedLists.java
+++ src/leetcode/23_Merge_k_Sorted_Lists/MergeKSortedLists.java
@@ -1,21 +1,97 @@
+import LeetcodeTestCase.TestCase;
+import LeetcodeTestCase.TestCaseExecutor;
+import LeetcodeTestCase.TestCaseFunctionGroup;
+import common.ListNode;
+import org.jetbrains.annotations.NotNull;
 
-public class MergeKSortedLists{
+import java.util.Arrays;
+
+public class MergeKSortedLists {
     // 23 Merge k Sorted Lists https://leetcode.com/problems/merge-k-sorted-lists
 
-    private static void bruteForce(){
+    private static ListNode bruteForce(ListNode[] lists) {
 
-            
-    }
-    private static void optimized1(){
-
-            
+return null;
     }
 
-    public static void main(String[] args){
+    private static ListNode optimized1(ListNode[] lists) {
+        /*
+        Time O(N)
+        Memory O(N)
+         */
+        ListNode ans = new ListNode(Integer.MIN_VALUE);
+        ListNode ref = ans;
 
-        // bruteForce();
-        // optimized1();
 
+        while (true) {
+            int lowestIndex = Integer.MAX_VALUE;
+            for (int i = 0; i < lists.length; i++) {
+                ListNode node = lists[i];
+                if (node == null) continue;
+                if (lowestIndex == Integer.MAX_VALUE) {
+                    lowestIndex = i;
+                } else {
+                    if (node.val < lists[lowestIndex].val  ) lowestIndex = i;
+                }
+            }
+            if (lowestIndex == Integer.MAX_VALUE) break;
+
+            ref.next = new ListNode(lists[lowestIndex].val);
+
+            lists[lowestIndex] = lists[lowestIndex].next;
+            ref = ref.next;
+
+
+        }
+
+        return ans.next;
+
+    }
+
+    public static void main(String[] args) {
+
+        new TestCaseExecutor(
+                computeTestCase(new int[][]{
+                        new int[]{1, 4, 5},
+                        new int[]{1, 3, 4},
+                        new int[]{2, 6}
+                }) ,
+
+                computeTestCase(new int[][]{
+                        new int[]{1, 6, 7},
+                        new int[]{-9, 3, 99},
+                        new int[]{1,4, 7,8}
+                }),
+                computeTestCase(new int[][]{
+                        new int[]{},
+                        new int[]{},
+                        new int[]{}
+                })
+
+
+        );
+
+    }
+
+    @NotNull
+    private static TestCase computeTestCase(int[][] nums) {
+        return new TestCase(
+                "%s ".formatted(
+                        Arrays.toString(Arrays.stream(nums).map(lists -> Arrays.toString(lists)).toArray(String[]::new))
+                ),
+
+/*
+                new TestCaseFunctionGroup("bruteForce", () -> {
+                    var ans = bruteForce(ListNode.create(nums1), ListNode.create(nums2));
+                    return ans != null ? ans.toString() : "";
+                }),
+                */
+
+                new TestCaseFunctionGroup("optimized1", () -> {
+                    var ans = optimized1(Arrays.stream(nums).map(lists -> ListNode.create(lists)).toArray(ListNode[]::new));
+                    return ans != null ? ans.toString() : "";
+                })
+        );
     }
 
 
