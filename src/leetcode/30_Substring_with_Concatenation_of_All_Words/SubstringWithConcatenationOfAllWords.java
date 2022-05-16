6b93d482e3c67aa5c45b7ba0a1da835a75dc04e1
ayotycoon <kaysehventeen@gmail.com>
2022-05-16 16:45:37 -0500

    added more sols

--- src/leetcode/30_Substring_with_Concatenation_of_All_Words/SubstringWithConcatenationOfAllWords.java
+++ src/leetcode/30_Substring_with_Concatenation_of_All_Words/SubstringWithConcatenationOfAllWords.java
@@ -37,51 +37,72 @@ public class SubstringWithConcatenationOfAllWords {
         return ans;
     }
 
-    private static List<Integer> optimized1(String s, String[] words, int stringIndex) {
-    /*
-     Time O(1)
-     Space O(1)
+//    private static List<Integer> optimized1(String s, String[] words, int stringIndex) {
+//    /*
+//     Time O(1)
+//     Space O(1)
+//    */
+//
+//
+//        List<Integer> ans = new ArrayList<>();
+//        if (words.length == 0) return ans;
+//        if (stringIndex >= s.length()) return null;
+//
+//
+//        for (int i = 0; i < words.length; i++) {
+//            var curr = words[i];
+//            if (s.substring(stringIndex).startsWith(curr)) {
+//
+//                String[] wordsMod = new String[words.length - 1];
+//                int v = 0;
+//                for (int j = 0; j < words.length; j++) {
+//                    if (i == j) continue;
+//                    wordsMod[v] = words[j];
+//                    v++;
+//                }
+//                var temp = optimized1(s, wordsMod, stringIndex + curr.length());
+//                if (temp != null && words.length == 2) {
+//                    ans.addAll(temp);
+//                }
+//            }
+//        }
+//
+//        var temp = words.length != 0 ? optimized1(s, words, stringIndex + words[0].length()) : null;
+//        if (temp != null && words.length == 2) ans.addAll(temp);
+//
+//
+//        return ans;
+//    }
+//
+//    private static List<Integer> optimized1(String s, String[] words) {
+//        return optimized1(s, words, 0);
+//
+//    }
+
+    private static List<Integer> optimized2(String s, String[] words) {
+            /*
+     Time O(N*M*len)
+     Space O(M=N)
     */
-
-
-        List<Integer> ans = new ArrayList<>();
-        if (words.length == 0) return ans;
-        if (stringIndex >= s.length()) return null;
-
-
-        for (int i = 0; i < words.length; i++) {
-            var curr = words[i];
-            if (s.substring(stringIndex).startsWith(curr)) {
-
-                String[] wordsMod = new String[words.length - 1];
-                int v = 0;
-                for (int j = 0; j < words.length; j++) {
-                    if (i == j) continue;
-                    wordsMod[v] = words[j];
-                    v++;
-                }
-                var temp = optimized1(s, wordsMod, stringIndex + curr.length());
-                if (temp != null && words.length == 2) {
-                    temp.add(0, stringIndex);
-                    System.out.println(temp);
-                    System.out.println(Arrays.toString(words));
-                    System.out.println("\n");
-                    ans.addAll(temp);
-                }
+        Map<String, Integer> map = new HashMap();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        List<Integer> result = new ArrayList();
+    
+        int eachWordLength = words[0].length();
+        for (int i = 0; i <= s.length() - (words.length * eachWordLength); i++) {
+            Map<String, Integer> seen = new HashMap<>();
+            for (int j = 0; j < words.length; j++) {
+                int nextIndexOfWord = i + j * eachWordLength;
+                String nextWord = s.substring(nextIndexOfWord, nextIndexOfWord + eachWordLength);
+                if (!map.containsKey(nextWord))break;
+                seen.put(nextWord, seen.getOrDefault(nextWord, 0) + 1);
+                if (seen.get(nextWord) > map.getOrDefault(nextWord, 0)) break;
+                if (j + 1 == words.length) result.add(i);
             }
         }
-
-            var temp = words.length != 0 ? optimized1(s, words, stringIndex + words[0].length()): null;
-            if(temp!= null && words.length == 2) ans.addAll(temp);
-
-
-
-        return ans;
-    }
-
-    private static List<Integer> optimized1(String s, String[] words) {
-        return optimized1(s, words, 0);
-
+        return result;
     }
 
     public static void main(String[] args) {
@@ -110,8 +131,12 @@ public class SubstringWithConcatenationOfAllWords {
                 }),
 
 
-                new TestCaseFunctionGroup("optimized1", () -> {
-                    var ans = optimized1(s, words);
+//                new TestCaseFunctionGroup("optimized1", () -> {
+//                    var ans = optimized1(s, words);
+//                    return ans.toString();
+//                }),
+                new TestCaseFunctionGroup("optimized2", () -> {
+                    var ans = optimized2(s, words);
                     return ans.toString();
                 })
         );
--- src/leetcode/31_Next_Permutation/NextPermutation.java
+++ src/leetcode/31_Next_Permutation/NextPermutation.java
@@ -1,6 +1,11 @@
+import LeetcodeTestCase.TestCase;
+import LeetcodeTestCase.TestCaseExecutor;
+import LeetcodeTestCase.TestCaseFunctionGroup;
+import org.jetbrains.annotations.NotNull;
 import utils.Permutate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,37 +13,113 @@ import java.util.stream.Collectors;
 public class NextPermutation {
     // 31 Next Permutation https://leetcode.com/problems/next-permutation
 
-    private static void bruteForce() {
-/*        
-Time O(1)
- Space O(1)
-*/
 
+    public static List<String> getAll(int[] nums) {
+        List<String> ans = new ArrayList<>();
+        if (nums.length == 0) {
+            ans.add("");
+            return ans;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            var curr = nums[i];
+            int[] wordsMod = new int[nums.length - 1];
+
+            int v = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) continue;
+                wordsMod[v] = nums[j];
+                v++;
+            }
+
+
+            var temp = getAll(wordsMod).stream().map(x -> curr + x).collect(Collectors.toList());
+            ans.addAll(temp);
+
+        }
+        return ans;
+    }
+
+
+    private static void bruteForce(int[] nums) {
+/*        
+Time O(N^N)
+ Space O(N)
+*/
+        String[] ans = new String[0];
+        var sorted = Arrays.copyOf(nums,nums.length);
+        Arrays.sort(sorted);
+        StringBuilder sb = new StringBuilder();
+        for(int n: nums) sb.append(n);
+        String str = sb.toString();
+        var values = getAll(sorted);
+
+        for(int i = 0; i < values.size(); i++){
+            if(values.get(i).equals(str)){
+                if(i+1 < values.size()){
+                    ans = values.get(i+1).split("");
+                }else {
+                    ans = values.get(0).split("");
+                }
+
+                break;
+
+            }
+
+        }
+
+        for(int i =0; i < ans.length; i++){
+            nums[i] = Integer.parseInt(ans[i]);
+        }
 
     }
 
-    private static List<List<Integer>> optimized1(int[] nums) {
+    private static void optimized1(int[] nums) {
 /*        
 Time O(1)
  Space O(1)
 */
-        LinkedList<List<Integer>> list = new LinkedList<>();
+
         List<List<Integer>> ans = new ArrayList<>();
-        list.add(new ArrayList<>());
 
 
 
-        return ans;
     }
 
 
     public static void main(String[] args) {
 
-        // bruteForce();
-        // optimized1();
 
-        System.out.println(Permutate.getAll(new String[]{"1", "2", "3","4"}));
+        new TestCaseExecutor(
+                computeTestCase(new int[]{1,2,3}),
+                computeTestCase(new int[]{3,2,1})
 
+
+        );
+
+    }
+
+    @NotNull
+    private static TestCase computeTestCase(int[] nums) {
+        return new TestCase(
+                "%s ".formatted(
+                         Arrays.toString(nums)
+                ),
+                new TestCaseFunctionGroup("bruteForce", () -> {
+                    var copy = Arrays.copyOf(nums,nums.length);
+                    bruteForce( copy);
+                    return Arrays.toString(copy);
+                }),
+
+
+                new TestCaseFunctionGroup("optimized1", () -> {
+                    var copy = Arrays.copyOf(nums,nums.length);
+                  optimized1( copy);
+                     return Arrays.toString(copy);
+                })
+
+        );
     }
 
 
