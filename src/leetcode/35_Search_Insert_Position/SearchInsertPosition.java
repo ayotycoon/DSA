afa14bd879df1bb57fc3c110c0f627ccc949b715
ayotycoon <kaysehventeen@gmail.com>
2022-05-18 22:32:08 -0500

    added more sols

--- src/leetcode/35_Search_Insert_Position/SearchInsertPosition.java
+++ src/leetcode/35_Search_Insert_Position/SearchInsertPosition.java
@@ -1,29 +1,90 @@
+import LeetcodeTestCase.TestCase;
+import LeetcodeTestCase.TestCaseExecutor;
+import LeetcodeTestCase.TestCaseFunctionGroup;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 public class SearchInsertPosition{
     // 35 Search Insert Position https://leetcode.com/problems/search-insert-position
 
-    private static void bruteForce(){
+    private static int bruteForce(int[] nums, int target){
 /*        
-Time O(1)
+Time O(N)
  Space O(1)
 */
+        if(nums.length == 0) return 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) return i;
 
+            if(nums[i] > target) return i;
+
+        }
+
+
+        return nums.length;
             
     }
-    private static void optimized1(){
+    private static int optimized1(int[] nums, int target){
 /*        
-Time O(1)
+Time O(logN)
  Space O(1)
 */
+        if(nums.length == 0) return 0;
+        if(target > nums[nums.length-1]) return nums.length;
+        int i = 0;
+        int j = nums.length-1;
+        while (i<=j) {
 
-            
+            int midIndex = (i+j) / 2;
+            int mid = nums[midIndex];
+            if (mid == target) {
+                return midIndex;
+
+
+            } else if (target < mid) {
+                j = midIndex-1;
+            } else {
+                i = midIndex+1;
+            }
+
+        }
+
+            return j+1;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        // bruteForce();
-        // optimized1();
 
+        new TestCaseExecutor(
+                computeTestCase(new int[]{1,3,5,6}, 5),
+                computeTestCase(new int[]{1,3,5,7}, 2),
+                computeTestCase(new int[]{1,3,5,6}, 7)
+
+
+        );
+
+    }
+
+
+    @NotNull
+    private static TestCase computeTestCase(int[] nums, int target) {
+        return new TestCase(
+                "%s | %d".formatted(
+                        Arrays.toString(nums), target
+                ),
+                new TestCaseFunctionGroup("bruteForce", () -> {
+
+                    return bruteForce(nums, target)+"";
+
+                }),
+
+                new TestCaseFunctionGroup("optimized1", () -> {
+                    return optimized1(nums, target)+"";
+                })
+
+
+        );
     }
 
 
--- src/leetcode/36_Valid_Sudoku/ValidSudoku.java
+++ src/leetcode/36_Valid_Sudoku/ValidSudoku.java
@@ -1,30 +1,124 @@
+import LeetcodeTestCase.TestCase;
+import LeetcodeTestCase.TestCaseExecutor;
+import LeetcodeTestCase.TestCaseFunctionGroup;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
 
 public class ValidSudoku{
     // 36 Valid Sudoku https://leetcode.com/problems/valid-sudoku
 
-    private static void bruteForce(){
+    private static int getGrid(int row, int column){
+        if(row >= 0 && row <=2 && column >=0 && column <=2) return 0;
+        if(row >= 0 && row <=2 && column >=3 && column <=5) return 1;
+        if(row >= 0 && row <=2 && column >=6 && column <=8) return 2;
+        if(row >= 3 && row <=5 && column >=0 && column <=2) return 3;
+        if(row >= 3 && row <=5 && column >=3 && column <=5) return 4;
+        if(row >= 3 && row <=5 && column >=6 && column <=8) return 5;
+        if(row >= 6 && row <=8 && column >=0 && column <=2) return 6;
+        if(row >= 6 && row <=8 && column >=3 && column <=5) return 7;
+        if(row >= 6 && row <=8 && column >=6 && column <=8) return 8;
+
+        return -1;
+
+    }
+
+    private static boolean bruteForce(char[][] board){
+/*        
+Time O(NM)
+ Space O(N^2M)
+*/
+         List<Set<Character>> rowSet = new ArrayList(9);
+         List<Set<Character>> gridSet = new ArrayList(9);
+        for(int i=0; i <9 ; i++){
+            rowSet.add(new HashSet<>());
+            gridSet.add(new HashSet<>());
+        }
+
+        for(int row = 0; row < board.length; row++){
+            Set<Character> columnSet = new HashSet<>();
+            for(int column = 0; column < board[row].length; column++){
+                Character curr = board[row][column];
+                if(curr == '.') continue;
+
+                if(columnSet.contains(curr)) return false;
+                columnSet.add(curr);
+
+                if(rowSet.get(column).contains(curr)) return false;
+                rowSet.get(column).add(curr);
+
+                int grid = getGrid(row,column);
+
+                if(gridSet.get(grid).contains(curr)) return false;
+                gridSet.get(grid).add(curr);
+
+
+
+
+            }}
+
+           return true;
+    }
+    private static boolean optimized1(char[][] board){
 /*        
 Time O(1)
  Space O(1)
 */
-
-            
-    }
-    private static void optimized1(){
-/*        
-Time O(1)
- Space O(1)
-*/
-
+return false;
             
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        // bruteForce();
-        // optimized1();
+
+        new TestCaseExecutor(
+                computeTestCase(new char[][]{new char[]{'5','3','.','.','7','.','.','.','.'}
+                        ,new char[]{'6','.','.','1','9','5','.','.','.'}
+                        ,new char[]{'.','9','8','.','.','.','.','6','.'}
+                        ,new char[]{'8','.','.','.','6','.','.','.','3'}
+                        ,new char[]{'4','.','.','8','.','3','.','.','1'}
+                        ,new char[]{'7','.','.','.','2','.','.','.','6'}
+                        ,new char[]{'.','6','.','.','.','.','2','8','.'}
+                        ,new char[]{'.','.','.','4','1','9','.','.','5'}
+                        ,new char[]{'.','.','.','.','8','.','.','7','9'}}),
+                computeTestCase(
+                        new char[][]{new char[]{'8','3','.','.','7','.','.','.','.'}
+                        ,new char[]{'6','.','.','1','9','5','.','.','.'}
+                        ,new char[]{'.','9','8','.','.','.','.','6','.'}
+                        ,new char[]{'8','.','.','.','6','.','.','.','3'}
+                        ,new char[]{'4','.','.','8','.','3','.','.','1'}
+                        ,new char[]{'7','.','.','.','2','.','.','.','6'}
+                        ,new char[]{'.','6','.','.','.','.','2','8','.'}
+                        ,new char[]{'.','.','.','4','1','9','.','.','5'}
+                        ,new char[]{'.','.','.','.','8','.','.','7','9'}}
+                )
+
+
+        );
 
     }
+
+
+    @NotNull
+    private static TestCase computeTestCase(char[][] board) {
+        return new TestCase(
+                "%s".formatted(
+                        Arrays.toString(board)
+                ),
+                new TestCaseFunctionGroup("bruteForce", () -> {
+
+                    return bruteForce(board)+"";
+
+                }),
+
+                new TestCaseFunctionGroup("optimized1", () -> {
+                    return optimized1(board)+"";
+                })
+
+
+        );
+    }
+
 
 
 }
