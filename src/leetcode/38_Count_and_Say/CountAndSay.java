2e4d237e126a941ba0feb67ffe6f9ac8d679fcb6
ayotycoon <kaysehventeen@gmail.com>
2022-05-19 07:53:41 -0500

    added more sols

--- src/leetcode/38_Count_and_Say/CountAndSay.java
+++ src/leetcode/38_Count_and_Say/CountAndSay.java
@@ -1,30 +1,91 @@
+import LeetcodeTestCase.TestCase;
+import LeetcodeTestCase.TestCaseExecutor;
+import LeetcodeTestCase.TestCaseFunctionGroup;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 public class CountAndSay{
     // 38 Count and Say https://leetcode.com/problems/count-and-say
 
-    private static void bruteForce(){
+    private static String bruteForce(int n){
 /*        
 Time O(1)
  Space O(1)
 */
-
+return "";
             
     }
-    private static void optimized1(){
+    private static String optimized1(int n){
 /*        
-Time O(1)
- Space O(1)
+Time O(N)
+ Space O(N)
 */
 
-            
+        if(n == 1) return "1";
+
+        String prev = optimized1(n-1);
+        StringBuilder sb = new StringBuilder();
+
+        char curr = 'x';
+        int index = 0;
+        for(int i = 0; i < prev.length();i++){
+            curr = prev.charAt(i);
+            if(i == 0){
+                index++;
+                if(i == prev.length()-1){
+                    sb.append(index+""+curr);
+                }
+                continue;
+            }
+            if(prev.charAt(i) == prev.charAt(i-1)){
+                index++;
+            }else {
+                sb.append(index+""+prev.charAt(i-1));
+                index = 1;
+
+            }
+
+            if(i == prev.length()-1){
+                sb.append(index+""+curr);
+            }
+        }
+        return sb.toString();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        // bruteForce();
-        // optimized1();
+
+        new TestCaseExecutor(
+                computeTestCase(1),
+                computeTestCase(4)
+
+
+        );
 
     }
+
+
+    @NotNull
+    private static TestCase computeTestCase(int n) {
+        return new TestCase(
+                "%d".formatted(
+                        n
+                ),
+                new TestCaseFunctionGroup("bruteForce", () -> {
+
+                    return bruteForce(n);
+
+                }),
+
+                new TestCaseFunctionGroup("optimized1", () -> {
+                    return optimized1(n);
+                })
+
+
+        );
+    }
+
 
 
 }
