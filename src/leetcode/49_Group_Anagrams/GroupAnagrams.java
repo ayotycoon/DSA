e3559ff3fb2ab267b22fa61db9f68a312a732967
ayotycoon <kaysehventeen@gmail.com>
2022-05-30 20:06:39 -0500

    added 50

--- src/leetcode/49_Group_Anagrams/GroupAnagrams.java
+++ src/leetcode/49_Group_Anagrams/GroupAnagrams.java
@@ -1,29 +1,74 @@
+import LeetcodeTestCase.TestCase;
+import LeetcodeTestCase.TestCaseExecutor;
+import LeetcodeTestCase.TestCaseFunctionGroup;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GroupAnagrams{
     // 49 Group Anagrams https://leetcode.com/problems/anagrams
 
-    private static void bruteForce(){
+    private static List<List<String>> bruteForce(String[] strs){
+/*        
+Time O((N * MLogM)
+ Space O(M*N)
+*/
+        Map<String,List<String>> map = new HashMap<>();
+
+        for(String str: strs){
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedS = str.equals("") ? "" : Arrays.toString(charArray);
+            if(!map.containsKey(sortedS)) map.put(sortedS, new ArrayList<>());
+            map.get(sortedS).add(str);
+        }
+
+       return map.values().stream().collect(Collectors.toList());
+    }
+    private static List<List<String>> optimized1(String[] strs){
 /*        
 Time O(1)
  Space O(1)
 */
 
-            
-    }
-    private static void optimized1(){
-/*        
-Time O(1)
- Space O(1)
-*/
-
-            
+            return new ArrayList<>();
     }
 
-    public static void main(String[] args){
 
-        // bruteForce();
-        // optimized1();
+    public static void main(String[] args) {
 
+        new TestCaseExecutor(
+
+                computeTestCase(new String[]{
+                        "eat","tea","tan","ate","nat","bat"
+                }),
+                computeTestCase(new String[]{
+                        ""
+                })
+
+        );
+
+    }
+
+    @NotNull
+    private static TestCase computeTestCase(String[] strs) {
+        return new TestCase(
+                "%s".formatted(
+                         Arrays.toString(strs)
+                ),
+
+                new TestCaseFunctionGroup("bruteForce", () -> {
+
+                    return bruteForce(strs).toString();
+                }),
+                new TestCaseFunctionGroup("optimized1", () -> {
+
+                    return optimized1(strs).toString();
+                })
+
+
+        );
     }
 
 
--- src/leetcode/50_PowXN/PowXN.java
+++ src/leetcode/50_PowXN/PowXN.java
@@ -1,30 +1,83 @@
+import LeetcodeTestCase.TestCase;
+import LeetcodeTestCase.TestCaseExecutor;
+import LeetcodeTestCase.TestCaseFunctionGroup;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PowXN{
     // 50 "Pow(x, n)" https://leetcode.com/problems/powx-n
 
-    private static void bruteForce(){
+    private static double bruteForce(double x, int n){
+    /*
+    Time O(n)
+     Space O(1)
+    */
+        if(x == 1) return 1d;
+        if(x == 0) return 0d;
+        boolean isNNeg = n <0;
+        n = Math.abs(n);
+        double ans = 1d;
+        for(int i =0; i < n; i++)ans*=x;
+        if(isNNeg) return 1/ans;
+        return ans;
+    }
+    private static double optimized1(double x, int n, Map<Integer,Double> memo){
 /*        
-Time O(1)
- Space O(1)
+Time O(logN)
+ Space O(logN)
 */
 
-            
-    }
-    private static void optimized1(){
-/*        
-Time O(1)
- Space O(1)
-*/
+        if(memo.containsKey(n)) return memo.get(n);
+        if(n == 0) return 1d;
+        if(n == 1) return x;
+        if(n == -1) return (double)(1/x);
+        boolean isNNeg = n <0;
+        double ans = optimized1(x, n/2,memo) * optimized1(x, n/2,memo);
+        if((n%2) != 0){
+            // is odd
+            ans *= optimized1(x, isNNeg ? -1 : 1,memo);
+        }
+        memo.put(n,ans);
+        return ans;
 
-            
-    }
-
-    public static void main(String[] args){
-
-        // bruteForce();
-        // optimized1();
 
     }
+
+
+    public static void main(String[] args) {
+
+        new TestCaseExecutor(
+
+                computeTestCase(34.00515d, -3 )
+
+        );
+
+    }
+
+    @NotNull
+    private static TestCase computeTestCase(double x, int n) {
+        return new TestCase(
+                "%s | %s".formatted(
+                        x+"",n+""
+                ),
+
+                new TestCaseFunctionGroup("bruteForce", () -> {
+
+                    return bruteForce(x,n)+"";
+                }),
+                new TestCaseFunctionGroup("optimized1", () -> {
+
+                    return optimized1(x,n, new HashMap<>())+"";
+                })
+
+
+        );
+    }
+
+
 
 
 }
